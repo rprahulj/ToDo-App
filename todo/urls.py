7501c2872d0ed2dd0bc7204e2afc31025d47f27d
Rahul Patel <rprahulj@gmail.com>
2025-09-13 19:52:09 +0530

    updated todo app

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',  views.task, name='home'),
     path('add_task', views.add_task, name='add_task'),
-    path('delete_task/<id>/', views.delete_task, name ='delete_task')
+    path('delete_task/<id>/', views.delete_task, name ='delete_task'),
+    path('mark_task/<id>/', views.mark_task, name = 'mark_task'),
+    path('unmark_task/<id>/', views.unmark_task, name = 'unmark_task'),
+    path('update_task/<id>/', views.update_task, name = 'update_task'),
     ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -23,6 +23,33 @@ def add_task(request):
     return render(request, 'add_task.html')
 
 def delete_task(request, id):
-    task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect('home')
+
+def mark_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.completed = True
+    task.save()
+    return redirect('home')
+
+def unmark_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.completed = False
+    task.save()
+    return redirect('home')
+
+def update_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        task_description = request.POST.get('task_description')
+
+        task.title = title
+        task.task_description = task_description
+
+        task.save()
+        return redirect('home')
+    
+    context = {'task': task}
+    return render(request, 'update_task.html', context)
